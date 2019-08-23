639b9a451cd959d1dffb4184798d8cfeb4c256e7
CScori <amilanotte1366@gmail.com>
2019-08-22 20:46:17 -0500

    addsformik vlaue map for login - still no new sign up

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,15 +1,32 @@
 import React from 'react'
 import { withFormik,Form, Feild } from "formik";
 
-const Login = () => {
+const LoginForm = () => {
     return (
        <Form>
            <Field type="email" name="email" placeholder="Email" />
-           <Field type="username" name="username" placeholder="UserName" />
+           <Field type="username" name="username" placeholder="Username" />
            <Field type="password" name="password" placeholder="Placeholder" />
-           <button>Submit</button> /*this should link to tabnav*/
+           <button>Submit</button> 
        </Form>
+       /*button submit --> this should link to tabnav*/
     );
 }
+const FormikLoginForm = withFormik({
+    mapPropsToValues({ email, username, password }) {
+      return {
+        email: email || "",
+        username: username || "",
+        password: password || ""
+      };
+    },
+  
+    handleSubmit(values) {
+      console.log(values);
+      //THIS IS WHERE YOU DO YOUR FORM SUBMISSION CODE... HTTP REQUESTS, ETC.
+    }
+  })(LoginForm);
+  
+  export default FormikLoginForm;
 
 export default Login
