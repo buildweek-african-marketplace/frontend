1b694b2e99c8c88cd1c4269ed214906fcbad1b23
CScori <amilanotte1366@gmail.com>
2019-08-22 20:40:56 -0500

    adds inital input for login...loking for a new login option

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import { withFormik,Form, Feild } from "formik";
 
 const Login = () => {
     return (
-        <div>
-            
-        </div>
-    )
+       <Form>
+           <Field type="email" name="email" placeholder="Email" />
+           <Field type="username" name="username" placeholder="UserName" />
+           <Field type="password" name="password" placeholder="Placeholder" />
+           <button>Submit</button> /*this should link to tabnav*/
+       </Form>
+    );
 }
 
 export default Login
