3a629ab3a6a1f9a617d8d38076d2917acd24d855
CScori <amilanotte1366@gmail.com>
2019-08-26 22:47:27 -0500

    adds nonformik form

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,10 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+
 
 const Login = () => {
+    const [user, setUser] = useState({ username: "", password: "" });
+
+const handleChange = event => {
+  setUser({ ...user, [event.target.name]: event.target.value });
+};
+
+const handleSubmit = event => {
+  event.preventDefault();
+  console.log(user.name);
+  console.log(user.password);
+};
     return (
-        <div>
-            
-        </div>
+       <div>
+ <form onSubmit={event => handleSubmit(event)}>
+        <label>
+          Username:
+          <input
+            type="text"
+            name="username"
+            value={user.username}
+            onChange={event => handleChange(event)}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="text"
+            name="password"
+            value={user.password}
+            onChange={event => handleChange(event)}
+          />
+        </label>
+        <button>Submit!</button>
+      </form>
+       </div>
     )
 }
 
--- frontend/src/components/SaleForm.js
+++ frontend/src/components/SaleForm.js
@@ -3,7 +3,8 @@ import React from 'react'
 const SaleForm = () => {
     return (
         <div>
-            
+            <h1>Sanity
+            </h1>
         </div>
     )
 }
