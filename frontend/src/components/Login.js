cd595e838bdc2bfea4cb8c7fd2ab7d6fc391ce11
CScori <amilanotte1366@gmail.com>
2019-08-26 22:35:51 -0500

    restart login code

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,35 +1,12 @@
 import React from 'react'
-import { withFormik, Form, Field } from "formik";
-import { Link, Route } from "react-router-dom";
-import TabNav from "./TabNav";
 
-
-const LoginForm = () => {
+const Login = () => {
     return (
-        <Form>
-            <Field type="email" name="email" placeholder="Email" />
-            <Field type="username" name="username" placeholder="Username" />
-            <Field type="password" name="password" placeholder="Placeholder" />
-            <Link to="/home">
-                <button>Submit</button>
-            </Link>
-            <Route path="/home" component={TabNav} />
-        </Form>
-        
-    );
+        <div>
+            
+        </div>
+    )
 }
-const FormikLoginForm = withFormik({
-    mapPropsToValues({ email, username, password }) {
-        return {
-            email: email || "",
-            username: username || "",
-            password: password || ""
-        };
-    },
 
-    handleSubmit(values) {
-        console.log(values);
-    }
-})(LoginForm);
+export default Login
 
-export default LoginForm
