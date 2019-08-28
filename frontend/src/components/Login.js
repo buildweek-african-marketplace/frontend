2b091a4d2a8a922032fe9e4739ad9a244a9bb576
CScori <amilanotte1366@gmail.com>
2019-08-27 22:56:43 -0500

    adds 99 login styling

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -42,6 +42,11 @@ const Login = () => {
       <p className='login-p'>Forgot your password?</p>
       <NavLink exact to="/welcome"> <button className="login-button">Sign In</button></NavLink>
       <Route exact path="/welcome" component={HomePage} />
+     <section className='login-hello'>
+       <h1 className='login-h1'>Hello, Friend!</h1>
+       <p className='hello'>Enter your personal details to get started!</p>
+       <button className='hello-button'>Sign Up</button>
+     </section>
     </div>
 
 
